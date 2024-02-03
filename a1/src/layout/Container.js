02303f066b1f682f359f63cea38a6d585ed70f27
Merim <merim.hasanbegovic@outlook.com>
2024-02-03 13:20:21 +0100

    fix: delete old frontend stuff

--- a1/src/layout/Container.js
+++
@@ -1,7 +0,0 @@
-const Container = ({ children, classes }) => (
-  <div className={`py-2 px-5 md:px-20 ${classes ? classes : ""}`}>
-    {children}
-  </div>
-);
-
-export default Container;
--- a1/src/layout/Footer.js
+++
@@ -1,7 +0,0 @@
-const Footer = () => (
-  <div className="text-center py-4 mt-2">
-    <p>merko &copy; 2019</p>
-  </div>
-);
-
-export default Footer;
